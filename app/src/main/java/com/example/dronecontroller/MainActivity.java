565397fbd4d728598eb7888e3a5f57e18ad7aa6c
G-Slim <38737110+G-Slim@users.noreply.github.com>
2020-03-18 23:33:21 -0600

    Solved all issues with networking. Networking tasks need to be executed on a different thread than the main one or else the application will crash. Ended up using a UDP connection instead of a TCP connection.

--- app/src/main/java/com/example/dronecontroller/MainActivity.java
+++ app/src/main/java/com/example/dronecontroller/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -14,19 +17,12 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BasicNetworking connection = null;
-        try {
-            connection = new BasicNetworking("192.168.4.1", 70);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        final BasicNetworking finalConnection = connection;
 
         // forward button event handler
         Button forward_button = findViewById(R.id.forward_button);
         forward_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(0);
+                sendCommand("0");
             }
         });
 
@@ -34,7 +30,7 @@ public class MainActivity extends AppCompatActivity{
         Button backward_button = findViewById(R.id.backward_button);
         backward_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(1);
+                sendCommand("1");
             }
         });
 
@@ -42,7 +38,7 @@ public class MainActivity extends AppCompatActivity{
         Button left_button = findViewById(R.id.left_button);
         left_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(2);
+                sendCommand("2");
             }
         });
 
@@ -50,7 +46,7 @@ public class MainActivity extends AppCompatActivity{
         Button right_button = findViewById(R.id.right_button);
         right_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(3);
+                sendCommand("3");
             }
         });
 
@@ -58,7 +54,7 @@ public class MainActivity extends AppCompatActivity{
         Button up_button = findViewById(R.id.up_button);
         up_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(4);
+                sendCommand("4");
             }
         });
 
@@ -66,7 +62,7 @@ public class MainActivity extends AppCompatActivity{
         Button down_button = findViewById(R.id.down_button);
         down_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(5);
+                sendCommand("5");
             }
         });
 
@@ -74,7 +70,7 @@ public class MainActivity extends AppCompatActivity{
         Button left_rot_button = findViewById(R.id.left_rot_button);
         left_rot_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(6);
+                sendCommand("6");
             }
         });
 
@@ -82,10 +78,37 @@ public class MainActivity extends AppCompatActivity{
         Button right_rot_button = findViewById(R.id.right_rot_button);
         right_rot_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finalConnection.SendCommand(7);
+                sendCommand("7");
+            }
+        });
+    }
+
+    private void sendCommand(final String num) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    // convert number to bytes
+                    byte buf[] = null;
+                    buf = num.getBytes();
+
+                    // create UDP socket
+                    DatagramSocket sock = new DatagramSocket();
+
+                    // address of WiFi module
+                    InetAddress ip = InetAddress.getByName("192.168.1.11");
+
+                    // create and send datagram
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length, ip, 8888);
+                    sock.send(packet);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+        thread.start();
     }
 }
 
